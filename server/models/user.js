791664b4e26bef449c8544e60deda1b893fad585
Heeyeong Kim <gldud9833@gmail.com>
2019-02-24 22:22:59 -0500

    Added user model

---
+++ server/models/user.js
@@ -0,0 +1,56 @@
+/*
+  <user.js>
+  COMP308-W2019-Midterm
+  Student Number: 300954759
+  Stduent Name: Heeyeong Kim
+  Date: 02/24/2019
+*/
+
+// require modules for our User Model
+let mongoose = require("mongoose");
+let passportLocalMongoose = require("passport-local-mongoose");
+
+let userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      default: "",
+      trim: true,
+      required: "username is required"
+    },
+    // password will be encrypted by passport-local-mongoose
+    email: {
+      type: String,
+      default: "",
+      trim: true,
+      required: "email is required"
+    },
+    displayName: {
+      type: String,
+      default: "",
+      trim: true,
+      required: "Display Name is required"
+    },
+    created: {
+      type: Date,
+      default: Date.now
+    },
+    update: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    collection: "users"
+  }
+);
+
+// configure options for the UserSchema
+
+let options = ({
+    missingPasswordError: "Wrong / Missing Password"
+});
+
+userSchema.plugin(passportLocalMongoose, options);
+
+module.exports.User = mongoose.model('User', userSchema);
